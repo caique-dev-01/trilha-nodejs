131ee81691fde5b77e89a0a70fd46a58cb414683
José Caique <caique.alves02@gmail.com>
2026-05-01 19:26:31 -0300

    ✨ feat: adiciona CRUD de autores e corrige controller de livros

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import livros from "./livrosRoutes.js";
+import autores from "./autoresRoutes.js";
 
 const routes = (app) => {
   app.use(express.json());
   app.route("/").get((req, res) => res.status(200).send("curso de node.js"));
   app.use(livros);
+  app.use(autores);
 };
 
 export default routes;
